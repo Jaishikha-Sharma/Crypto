bec17615b34ae62f774b63563d4e76c636b9f10e
Jaishikha-Sharma <jaishikhasharma05@gmail.com>
2025-01-09 12:12:49 +0530

    final

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import almalogo from '../components/Images/almalogo.png'
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    {/* <img src= {almalogo} alt ="Almabetter logo" />  */}
+    <h6><b>Crypto Market </b></h6>
+    </div>
+  
+</nav>
+    </>
+  )
+}
+
+export default Navbar
